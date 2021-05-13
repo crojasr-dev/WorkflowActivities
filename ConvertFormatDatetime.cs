225889c8c3a85c18288a65f2ef1a46a07543bd86
cristian.rojas <cristian.rojas@w-it.cl>
2021-05-13 19:42:39 -0400

    asdf

--- ConvertFormatDatetime.cs
+++ ConvertFormatDatetime.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
 
 namespace WorkflowActivities
 {
-    class ConvertFormatDatetime
+    public class ConvertFormatDatetime : CodeActivity
     {
+        [RequiredArgument]
+
+        [Input("DateTime input")]
+
+        public InArgument<DateTime> DateToEvaluate { get; set; }
+
+        [Output("Formatted DateTime output as string")]
+
+        public OutArgument<String> ForammtedDateTimeOutput { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(workflowContext.UserId);
+
+            DateTime utcDateTime = this.DateToEvaluate.Get(context);
+
+            if (utcDateTime.Kind != DateTimeKind.Utc)
+
+            {
+
+                utcDateTime = utcDateTime.ToUniversalTime();
+
+            }
+
+            var settings = service.Retrieve("usersettings", workflowContext.UserId, new ColumnSet("timezonecode"));
+
+            LocalTimeFromUtcTimeRequest timeZoneChangeRequest = new LocalTimeFromUtcTimeRequest()
+
+            {
+                UtcTime = utcDateTime,
+                TimeZoneCode = int.Parse(settings["timezonecode"].ToString())
+            };
+
+            LocalTimeFromUtcTimeResponse timeZoneResponse = service.Execute(timeZoneChangeRequest) as LocalTimeFromUtcTimeResponse;
+
+            this.ForammtedDateTimeOutput.Set(context, String.Format("{0:f}",timeZoneResponse.LocalTime));
+
+
+
+
+
+
+
+
+        }
     }
+
+
+
 }
